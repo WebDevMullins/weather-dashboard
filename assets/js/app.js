b51ca271d163c862307feb434629e3dab87e0828
KingOfExcuses <kingofexcusestv@gmail.com>
2023-09-08 16:33:43 -0500

    add app.js to html & fetch currentWeather to app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,14 @@
 require('dotenv').config({ path: '../../.env' })
+
+const API_KEY = process.env.API_KEY
+const defaultCity = 'dallas'
+
+const currentWeather = `https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${API_KEY}`
+
+fetch(currentWeather)
+	.then((res) => {
+		return res.json()
+	})
+	.then((data) => {
+		console.log(data)
+	})
